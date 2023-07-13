8a7628140c2531920e6cefab3bdd6b30927071af
raj kumar <rajkumar@rajs-MacBook-Pro.local>
2023-07-13 18:36:34 +0530

    code changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent },
+  // { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  // { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
